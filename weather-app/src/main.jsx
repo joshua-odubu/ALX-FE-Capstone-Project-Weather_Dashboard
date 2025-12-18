93328f7cb714c968efc07ed75c9789691a8e269a
joshua-odubu <freelance.odubu@gmail.com>
2025-12-18 13:56:50 +0000

    Fix GitHub Pages routing with basename

--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -1,13 +1,11 @@
-import "./tailwind.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/ALX-FE-Capstone-Project-Weather_Dashboard">
+    <App />
+  </BrowserRouter>
 );
