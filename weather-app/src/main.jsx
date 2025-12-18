5be0c952414f2ce6f9b133ed47f8b0a3ff9b82d4
joshua-odubu <freelance.odubu@gmail.com>
2025-12-18 13:58:35 +0000

    Fix CSS import for build

--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "./tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/ALX-FE-Capstone-Project-Weather_Dashboard">
